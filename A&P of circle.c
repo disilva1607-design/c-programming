bf33ff5b619ea26dfa01bac4d5134a9ee278d9ba
disilva1607-design <disilva1607@gmail.com>
2025-10-27 10:45:24 -0700

    Create A&P of circle.c

---
+++ A&P of circle.c
@@ -0,0 +1,13 @@
+//Arithmatic operators
+#include <stdio.h>
+int main()
+{
+5 int radius, perimeter,area;
+printf("enter the radius of the circle"); 
+scanf("%d", &radius);                                            
+perimeter=2*3.14*radius;
+ printf("The perimeter of the circle is %d", perimeter);
+ area=3.14*radius*radius;
+ printf("the area ofthe circle is %d", area);
+return 0;
+}
