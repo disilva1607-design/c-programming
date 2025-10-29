26eec5914171dbda69e159ad40ff70cdbeb99da3
disilva1607-design <disilva1607@gmail.com>
2025-10-29 07:53:36 -0700

    Create distance between two points.c

---
+++ distance between two points.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int x1, y1, x2, y2;
+    float z;
+    scanf("%d%d%d%d", &x1, &y1, &x2, &y2);
+    z = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+    printf("%.4f", z);
+    return 0;
+}
