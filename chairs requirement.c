7828faae99b906b6b221919cfcf8b8608eee2a62
disilva1607-design <disilva1607@gmail.com>
2025-10-29 00:45:52 -0700

    Create chairs requirement.c

---
+++ chairs requirement.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int X, Y;
+    scanf("%d%d", &X, &Y);
+    if (X < Y)
+    {
+        printf("0");
+    }
+    else
+    {
+        printf("%d", X - Y);
+    }
+    return 0;
+}
