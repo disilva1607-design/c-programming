b6518b9daa02c16163e5b8f05ff0f2b9ba456f54
disilva1607-design <disilva1607@gmail.com>
2025-10-28 23:09:27 -0700

    Create celsius to fahrenheit.c

---
+++ celsius to fahrenheit.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int celsius;
+    float fahrenheit;
+    scanf("%d",&celsius);
+    fahrenheit=(celsius*1.8)+32;
+    printf("%.2f",fahrenheit);
+    return 0;
+}
