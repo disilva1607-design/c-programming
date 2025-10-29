4613cbd54d156155efaa72f5a2fb0d2f114b0bca
disilva1607-design <disilva1607@gmail.com>
2025-10-29 07:36:22 -0700

    Create tickets.c

---
+++ tickets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int X, total_cost;
+    scanf("%d", &X);
+    total_cost = 4 * X;
+    if (total_cost <= 1000)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+    return 0;
+}
