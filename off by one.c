3827b247ad8820e324575e610a9eaa883e078237
disilva1607-design <disilva1607@gmail.com>
2025-10-28 23:34:01 -0700

    Create off by one.c

---
+++ off by one.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int A,B,result;
+    scanf("%d%d",&A,&B);
+    result=(A+B);
+    printf("%d",result); // The format specifier in the image shows "%d1", which is likely a typo and interpreted as "%d" for integer output.
+    return 0;
+}
