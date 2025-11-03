6d2352e6c1fe5c6f3157f6e12e941f878332626f
disilva1607-design <disilva1607@gmail.com>
2025-11-03 12:31:43 +0530

    Create donation rewards.c

---
+++ donation rewards.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    if (n <= 3)
+        printf("BRONZE");
+    else if (n > 3 && n <= 6)
+        printf("SILVER");
+    else
+        printf("GOLD");
+    return 0;
+}
