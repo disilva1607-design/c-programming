f0021a1a488a75d1f714d5a70112074b3889ec69
disilva1607-design <disilva1607@gmail.com>
2025-11-03 20:17:27 +0530

    Create climate.c

---
+++ climate.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    int temp;
+    scanf("%d", &temp);
+    if (temp > 20)
+    {
+        printf("HOT");
+    }
+    else
+    {
+        printf("COLD");
+    }
+    return 0;
+}
+
+
+
+
+
+
