c681f750b8a0402ede232eb0e361110391efa096
disilva1607-design <disilva1607@gmail.com>
2025-11-03 21:05:09 +0530

    Create grades.c

---
+++ grades.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int phy, chem, bio, math, comp;
+    float percentage;
+
+    // Input marks for 5 subjects
+    scanf("%d%d%d%d%d", &phy, &chem, &bio, &math, &comp);
+
+    // Calculate percentage
+    percentage = (phy + chem + bio + math + comp) / 5.0;
+
+    // Determine grade based on percentage
+    if (percentage >= 90) {
+        printf("Grade A");
+    } 
+    else if (percentage >= 80) {
+        printf("Grade B");
+    } 
+    else if (percentage >= 70) {
+        printf("Grade C");
+    } 
+    else if (percentage >= 60) {
+        printf("Grade D");
+    } 
+    else if (percentage >= 40) {
+        printf("Grade E");
+    } 
+    else {
+        printf("Grade F");
+    }
+
+    return 0;
+}
