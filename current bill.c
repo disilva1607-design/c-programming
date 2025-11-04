e75c27d243779f29444ac102f289c82378ddaa3a
disilva1607-design <disilva1607@gmail.com>
2025-11-04 06:45:32 +0530

    Create current bill.c

---
+++ current bill.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    int units;
+    float amount, surcharge, total;
+    scanf("%d", &units);
+    if (units <= 199)
+        amount = units * 1.20;
+    else if (units >= 200 && units < 400)
+        amount = units * 1.50;
+    else if (units >= 400 && units < 600)
+        amount = units * 1.80;
+    else
+        amount = units * 2.00;
+
+    if (amount > 400)
+        surcharge = amount * 0.15;
+    else
+        surcharge = 100.0;
+
+    total = amount + surcharge;
+    printf("%.2f", total);
+    return 0;
+}
