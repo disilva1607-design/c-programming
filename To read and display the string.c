79bbb3c032315cdeb7dc91ba99a82ac355326da5
disilva1607-design <disilva1607@gmail.com>
2025-11-16 17:30:31 +0530

    Create To read and display the string.c

---
+++ To read and display the string.c
@@ -0,0 +1,22 @@
+//read and display string
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	char name[10],roll[10],dept[10],sec[10];
+	puts("Enter your name");
+	gets(name);
+	puts("your name is");
+	puts(name);
+	puts("enter your roll");
+	gets(roll);
+	puts("your roll is");
+	puts(roll);
+	puts("enter your dept");
+	gets(dept);
+	puts("your dept is");
+	puts("dept");
+	printf("enter your sec:");
+	scanf("%d",&sec);
+return 0;
+}
